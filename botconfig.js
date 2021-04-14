9efda51e3923dda50d6bdde0cfe689cfc78fe6ec
sl-glitch <72424857+sl-glitch@users.noreply.github.com>
2021-04-14 07:47:46 +0545

    Update botconfig.js

--- botconfig.js
+++ botconfig.js
@@ -7,7 +7,7 @@ Show
 botConfig.js
 onboarding: Hey! Anonymous projects expire after 5 days. Create an account to save your progress.
 module.exports = {
-    token: "ODI0MjgzMTMzMjIxNTM1Nzg1.YFtHfQ.4MQfzRpA4t_HHB3tWP3qA9gy3os",
+    token: "BoT_TOKEN",
     prefix: ".",
     admins: [
         "PEOPLE WHO CAN USE ADD MONEY (IDS)"
